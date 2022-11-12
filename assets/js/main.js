404d84961070603cc0ccc86ce13ea14acfec5346
Matheus Tavares <mathtg2@gmail.com>
2022-11-12 00:43:00 -0300

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,48 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5
+let offset = 0;
 
-function convertPokemonToLi(pokemon) {
-  return `
-  <li class="pokemon ${pokemon.type}">
-  <span class="number">#${pokemon.number}</span>
-  <span class="name">${pokemon.name}</span>
+const maxRecords = 151
 
-  <div class="detail">
-    <ol class="types">
-          ${pokemon.types.map((type)=> `<li class="type ${type}">${type}</li>`).join('')}
-    </ol>
 
-    <img src="${pokemon.photo}" 
-      alt= "${pokemon.name}">
-  </div>
-  </li>
-  `
+
+function loadPokemonItens(offset, limit){
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon)=> `
+        <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+      
+        <div class="detail">
+          <ol class="types">
+                ${pokemon.types.map((type)=> `<li class="type ${type}">${type}</li>`).join('')}
+          </ol>
+      
+          <img src="${pokemon.photo}" 
+            alt= "${pokemon.name}">
+        </div>
+        </li>
+        `).join('')
+        pokemonList.innerHTML += newHtml
+  })
 }
 
+loadPokemonItens(offset, limit)
 
-const pokemonList = document.getElementById('pokemonList')
+loadMoreButton.addEventListener('click', () => {
+  offset += limit
 
-pokeApi.getPokemons().then((pokemons = []) => {
-  const newHtml = pokemons.map(convertPokemonToLi).join('')
-  pokemonList.innerHTML += newHtml
+  const qtdRecordNextPage = offset + limit
+
+  if(qtdRecordNextPage >= maxRecords) {
+    const newLimit =  maxRecords - offset
+    loadPokemonItens(offset, newLimit)
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+  } else {
+    loadPokemonItens(offset,limit)
+  }
+
 })
+
---
+++ teste.js
@@ -0,0 +1,12 @@
+
+
+
+
+let multiplicador = parseInt(prompt('Digite qual o número que deseja calcular'))
+
+
+for(let i = 1; i < 10; i++ ){
+  let resultado = multiplicador * i
+}
+
+alert('O resultado é = ' + )
